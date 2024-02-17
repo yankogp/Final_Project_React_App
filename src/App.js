ecf99bc2b94612a5dbe0c1e0c0f25d3c8e56b2dd
yankogp <146089512+yankogp@users.noreply.github.com>
2024-02-17 18:15:37 +0200

    9

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           First Deploy
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
