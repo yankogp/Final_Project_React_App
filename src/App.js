293fe7475a91dcac635e601115f4c1723162988e
yankogp <146089512+yankogp@users.noreply.github.com>
2024-02-19 17:34:02 +0200

    Deploy N1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Deploy N2
+           Deploy N1
           
         </p>
         <a
